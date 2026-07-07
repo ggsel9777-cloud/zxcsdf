7b951f3d8aa995ed253db6ff0622d7529b77251e
ggsel9777-cloud <ggsel9777@gmail.com>
2026-07-07 15:13:53 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
 #include <windows.h>
-#include <d3d12.h>
-#include <dxgi1_4.h>
 
-// Базовая логика для проверки работы DLL
 bool showMenu = false;
 
-// Функция, которая выполнится при успешном внедрении в игру
 DWORD WINAPI MainThread(LPVOID lpReserved) {
-    // Выводим сообщение, чтобы убедиться, что лаунчер сработал
-    MessageBoxA(NULL, "Скинчейнджер успешно загружен! Нажмите OK.", "CS2 Changer", MB_OK | MB_ICONINFORMATION);
+    MessageBoxA(NULL, "Successfully loaded! Press OK.", "CS2 Changer", MB_OK | MB_ICONINFORMATION);
     
     while (true) {
-        // Проверяем нажатие Правого Shift (VK_RSHIFT)
         if (GetAsyncKeyState(VK_RSHIFT) & 1) {
             showMenu = !showMenu;
-            // Звуковой сигнал при нажатии (высокий тон — включено, низкий — выключено)
             Beep(showMenu ? 800 : 500, 200); 
         }
         Sleep(10);
@@ -25,7 +18,6 @@ DWORD WINAPI MainThread(LPVOID lpReserved) {
 BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved) {
     if (dwReason == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hMod);
-        // Создаем поток внутри игры
         CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
     }
     return TRUE;
